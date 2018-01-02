ba80270909f9a3db27d239f132763dbf60a1cdff
ashutalewar <ashutalewar@gmail.com>
2018-01-02 15:58:34 +0530

    adding remote methods for model

--- server/models/message.js
+++ server/models/message.js
@@ -2,4 +2,24 @@
 
 module.exports = function(Message) {
 
+	Message.newmethod = function (name1,name2, cb) {
+		// body...
+		var date = new Date()
+		cb(null, "Hello " + name1 + " and " + name2, date)
+	}
+
+	Message.remoteMethod('newmethod', {
+	      accepts: [
+	  		{arg: 'name1', type: 'string'},
+	  		{arg: 'name2', type: 'string'}
+	      ],
+	      returns: [
+	      	{arg: 'greeting', type: 'string'},
+	      	{arg: 'date', type: 'date'}
+	      ],
+	      http: {
+	        path: '/newmethod',
+	        verb: 'post'
+	      },
+    });
 };
